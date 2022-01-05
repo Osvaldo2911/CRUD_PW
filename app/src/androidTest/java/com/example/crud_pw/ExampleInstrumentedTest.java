7b0c6afc6caf9df31eb0796fb5a0df60c6f6aa71
Osvaldo De la torre <ekobozz9@gmail.com>
2022-01-05 15:36:36 -0600

    Se modifico automaticamente

--- app/src/androidTest/java/com/example/crud_pw/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/crud_pw/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.crud_tbd;
+package com.example.crud_pw;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.crud_tbd", appContext.getPackageName());
+        assertEquals("com.example.crud_pw", appContext.getPackageName());
     }
 }
